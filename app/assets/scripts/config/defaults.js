298eb3eda5ad4e28323b6c2f433d98c040ff85aa
meomancer <meomancer@gmail.com>
2020-06-26 09:52:43 +0700

    update config

--- app/assets/scripts/config/defaults.js
+++ app/assets/scripts/config/defaults.js
@@ -11,5 +11,5 @@ export default {
   basemapStyleLink: 'mapbox://styles/derilinx/ck3yqjovg4dpn1crwajrc9ajr',
   geonodeUrl: 'http://127.0.0.1',
   country: COUNTRY,
-  subUrl: '/gep' // please don't use end trailing slash, example: /gep
+  subUrl: '' // please don't use end trailing slash, example: /gep
 };
--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -3,5 +3,6 @@
 export default {
   environment: 'production',
   baseUrl: 'http://localhost:9000',
-  dataServiceUrl: 'http://localhost:3000'
+  dataServiceUrl: '/gep-server',
+  subUrl: '/gep' // please don't use end trailing slash, example: /gep
 };
