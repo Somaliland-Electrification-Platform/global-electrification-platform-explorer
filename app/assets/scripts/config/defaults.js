5132628afd8711055c90376970f08c07f9dee96e
meomancer <meomancer@gmail.com>
2020-08-06 10:58:32 +0700

    update config

--- app/assets/scripts/config/defaults.js
+++ app/assets/scripts/config/defaults.js
@@ -1,15 +1,15 @@
 'use strict';
-const COUNTRY= 'Benin'
+const COUNTRY = 'Benin'
 export default {
-  appTitle: 'Benin Electrification Platform',
-  appShortTitle: 'BEP',
-  appDescription:
-    `Explore least cost electrification strategies in ${COUNTRY}.`,
-  baseUrl: 'http://localhost:9000',
-  dataServiceUrl: 'http://localhost:3000',
-  mapboxAccessToken: 'pk.eyJ1IjoiZGVyaWxpbngiLCJhIjoiY2szeTlzbWo2MDV6eDNlcDMxM3dzZXBieiJ9.zPf1iiFilYYwyx6ETNj_8w',
-  basemapStyleLink: 'mapbox://styles/derilinx/ck3yqjovg4dpn1crwajrc9ajr',
-  geonodeUrl: 'http://localhost',
-  country: COUNTRY,
-  subUrl: '' // please don't use end trailing slash, example: /gep
+    appTitle: `${COUNTRY} Electrification Platform`,
+    appShortTitle: `${COUNTRY[0]}EP`,
+    appDescription:
+        `Explore least cost electrification strategies in ${COUNTRY}.`,
+    baseUrl: 'http://localhost:9000',
+    dataServiceUrl: 'http://localhost:3000',
+    mapboxAccessToken: 'pk.eyJ1IjoiZGVyaWxpbngiLCJhIjoiY2szeTlzbWo2MDV6eDNlcDMxM3dzZXBieiJ9.zPf1iiFilYYwyx6ETNj_8w',
+    basemapStyleLink: 'mapbox://styles/derilinx/ck3yqjovg4dpn1crwajrc9ajr',
+    geonodeUrl: 'http://localhost',
+    country: COUNTRY,
+    subUrl: '' // please don't use end trailing slash, example: /gep
 };
